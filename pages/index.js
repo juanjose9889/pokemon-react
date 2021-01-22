e3a49a54a45d480219b3e8a508de1f646090f3ea
pabloVoorvaart <pablov@graficadirecta.com>
2021-01-22 12:11:34 +0100

    implemented list render, implemented main render logic

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import React, { useEffect, useState } from 'react';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [textlbl, setTextlbl] = useState("");
-  const [listaPokemon, setListaPokemon] = useState([]);
-  const [pokemon, setPokemon] = useState({});
+  const [listaPokemon, setListaPokemon] = useState([]); 
+ const [pokemon, setPokemon] = useState(null);
 
 
-  async function requestPoke() {
-    if (textlbl.length !== ''){
-      await fetch(`https://pokeapi.co/api/v2/pokemon/${textlbl}`)
-          .then(res => res.json().then((data) => {
-              console.log(data);
-              setPokemon(data);
-          }))
-      }
-  }
 
-  async function requestPokeList() {
-    if (textlbl.length !== '') {
-      await fetch(`https://pokeapi.co/api/v2/pokemon/`)
-          .then(res => res.json().then((data) => {
-              console.log(data);
-              setListaPokemon(data);
-          }))
-  }
+  /**
+   * Componente Lista
+   */
+
+  useEffect(() => {
+    requestPokeList();
+  }, []);
+  
+   async function requestPokeList() {
+    if (textlbl.length !== "") {
+      await fetch(`https://pokeapi.co/api/v2/pokemon/`).then((res) =>
+        res.json().then((data) => {
+          console.log(data);
+          setListaPokemon(data.results);
+        })
+      );
+    }
   }
 
   function render() {
-    return (<div>
-    {listaPokemon.map((pokemon) => {
-                    return(<p>{pokem.name}</p>)
-                    })
-                }
-    </div>);
+    return (
+      <div>
+        {listaPokemon.map((pokemon) => {
+          return <p>{pokemon.name}</p>;
+        })}
+      </div>
+    );
   }
 
-  useEffect(() =>{
-    requestPokeList()
-  },[])
+  /**
+   * Componente Busqueda
+   */
+
+  async function requestPoke() {
+    if (textlbl.length !== "") {
+      await fetch(`https://pokeapi.co/api/v2/pokemon/${textlbl}`).then((res) =>
+        res.json().then((data) => {
+          console.log(data);
+          setPokemon(data);
+        })
+      );
+    }
+  }
 
   return (
-  <div>
-     <input value = {textlbl} onChange={(evento) => {console.log(evento); setTextlbl(evento.target.value)}}/>
-      <button onClick={requestPoke}>
-      Click
-      </button>
-      
-      {pokemon !== {} ? 
-        <div>
-          {render()}
-        </div>
-        :
-        <div></div>
-      }
-  </div>
- 
-  
-  )
+    <div>
+      <input
+        value={textlbl}
+        onChange={(evento) => {
+          console.log(evento);
+          setTextlbl(evento.target.value);
+        }}
+      />
+      <button onClick={requestPoke}>Click</button>
+      {pokemon ? <div>{pokemon.name}</div> : render() }
+    </div>
+  );
 }
